7323575ebb22701bc71ee6a2cf909cc062abeec3
Jakob Devey <77177145+jakobdevey@users.noreply.github.com>
2023-11-24 09:52:57 -0500

    done for now...

--- extra_trees.py
+++ extra_trees.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import cv2
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.metrics import accuracy_score
