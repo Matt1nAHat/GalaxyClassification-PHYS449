abb9b9a2d00fe001c428963fc431d0a9cd8a97c6
Matthew Charbonneau <47362378+Matt1nAHat@users.noreply.github.com>
2023-11-26 22:21:38 -0500

    Python file that creates a feature vector for a given object ID

---
+++ featureVector.py
@@ -0,0 +1,68 @@
+import numpy as np
+import sdss 
+
+def calcModelColour(magnitudeDict):
+    """
+    Calculate model colour as the difference of adjacent filter's model magnitude.
+
+    Parameters:
+    modelMagnitudeDict (dict): A dictionary where the key is the filter name ('u', 'g', 'r', 'i', 'z') and the value is the corresponding model magnitude.
+
+    Returns:
+    dict: A dictionary where the key is the model colour name ('u-g', 'g-r', 'r-i', 'i-z') and the value is the corresponding model colour.
+    """
+    colorDict = {}
+    filters = ['u', 'g', 'r', 'i', 'z']
+    for i in range(len(filters) - 1):
+        colorDict[filters[i] + '-' + filters[i+1]] = magnitudeDict[filters[i]] - magnitudeDict[filters[i+1]]
+    return colorDict
+
+def calcConcentration(petroR90, petroR50):
+    """
+    Calculate the concentration indicies for each filter by taking the ratio of the petroR90 and petroR50 values.
+
+    Parameters:
+    petroR90, petroR50 (dict): The dictionaries.
+
+    Returns:
+    dict: A dictionary where the key is the same as the inputs, and the value is the ratio of the input values.
+    """
+    ratioDict = {}
+    for key in petroR90.keys():
+        ratioDict[key] = petroR90[key] / petroR50[key]
+    return ratioDict
+
+def getFeatures(obj_ID):
+    """
+    Get the features of the object from the SDSS database and create the feature vector.
+
+    Parameters:
+    obj_ID (int): The ID of the object.
+
+    Returns:
+    list: The feature vector.
+    """
+    ph = sdss.PhotoObj(obj_ID)
+    ph.cutout_image()
+    featureList = []
+    
+    #Append features to featureList
+    featureList.extend(calcModelColour(ph.fiberColour).values())
+    featureList.extend(calcModelColour(ph.modelColour).values())
+    featureList.extend(calcModelColour(ph.petroColour).values())
+    featureList.extend(calcConcentration(ph.petroR90,ph.petroR50).values())
+    featureList.extend(ph.secondMoment.values())
+    featureList.extend(ph.fourthMoment.values())
+    featureList.extend(ph.axisRatioDEV.values())
+    featureList.extend(ph.axisRatioEXP.values())
+    featureList.extend(ph.ellipticityE1.values())
+    featureList.extend(ph.ellipticityE2.values())
+    featureList.extend(ph.modelFitDEV.values())
+    featureList.extend(ph.modelFitEXP.values())
+    featureList.extend(ph.modelFitSTAR.values())
+
+    # Convert the feature list to a numpy array
+    feature_vector = np.array(featureList)
+
+    return feature_vector
+
