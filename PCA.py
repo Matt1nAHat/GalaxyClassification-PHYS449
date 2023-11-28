68074fe8ab69ecd92fa424780829fd54a71a9e51
Matthew Charbonneau <47362378+Matt1nAHat@users.noreply.github.com>
2023-11-28 18:00:58 -0500

    Finalized (hopefully) PCA for model testing: - Changed PCA to take the first 25 components as stated in the paper - Standardized features prior to PCA and normalized them after (using MinMaxScaler) -  Created a final_labels list which converts the probabilties in the original labels list into a single label for training

--- PCA.py
+++ PCA.py
@@ -1,3 +1,4 @@
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.decomposition import PCA
 import numpy as np
 
@@ -29,15 +30,34 @@ def performPCA(path = 'galaxyDataset.txt'):
     features = np.array(features)
     labels = np.array(labels)
 
-    # Assuming features is your feature vector
-    pca = PCA()
-    pca.fit(features[:,2:])
+    # Finalize the label array from probabilities to a single label
+    final_labels = []
+    for label in labels:
+        if label[1] == 'Star':
+            final_labels.append('Star')
+        else:
+            # Get the index of the maximum probability among the galaxy types
+            max_index = np.argmax(label[2:7])
+            if max_index == 0:
+                final_labels.append('Elliptical')
+            elif max_index in [1, 2, 3]:
+                final_labels.append('Spiral')
+            else:
+                final_labels.append('Merger')
 
-    cumulative_variance = np.cumsum(pca.explained_variance_ratio_)
-    n_components = np.where(cumulative_variance > 0.95)[0][0] + 1
+    # Convert the final_labels list to a numpy array
+    final_labels = np.array(final_labels)
 
-    # Now you can initialize a new PCA object with the determined number of components
-    pca = PCA(n_components=n_components)
-    pca.fit(features[:,2:])
-    transformed_features = pca.transform(features[:,2:])
-    return(transformed_features.shape)
+    # Standardize the features
+    scaler = StandardScaler()
+    standardized_features = scaler.fit_transform(features)
+
+    # Perform PCA
+    pca = PCA(n_components=25)
+    pca.fit(standardized_features)
+    transformed_features = pca.transform(standardized_features)
+
+    # Renormalize the PCA components
+    scaler = MinMaxScaler()
+    normalized_features = scaler.fit_transform(transformed_features)
+    return(normalized_features, final_labels)
