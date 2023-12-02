7f8379347878c8974d7195a4516618b6d87619b4
Jakob Devey <77177145+jakobdevey@users.noreply.github.com>
2023-12-01 19:15:08 -0500

    deleted unnecessary fies

--- dataProcessing/create_feature_vecs.py
+++
@@ -1,4 +0,0 @@
-from featureVector import saveFeatureVectors
-from featureVector import getFeatures
-
-saveFeatureVectors('dataAcquisition/Split_data_IDs/valid_50k.csv', 'dataProcessing/featureVectors/testFeatures_50k.txt')
