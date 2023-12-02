3694c7c6ced11a612a50e5a64ebc0b8b157c9bf4
Jakob Devey <77177145+jakobdevey@users.noreply.github.com>
2023-12-01 20:21:51 -0500

    split 85k ID`s for PCA

--- dataAcquisition/data_splitting.py
+++ dataAcquisition/data_splitting.py
@@ -2,9 +2,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 def main():
-    seed = 22
+    seed = 86
     # Load the CSV file into a DataFrame, skipping the first two rows
-    df = pd.read_csv('dataAcquisition\Object_IDs_and_classifications\ZooSpec\combined_50k.csv')
+    df = pd.read_csv('dataAcquisition\Object_IDs_and_classifications\ZooSpec\combined_85k.csv')
 
     # Shuffle the DataFrame
     df = df.sample(frac=1, random_state=seed) # set seed for random splitting
@@ -14,9 +14,9 @@ def main():
     valid_df, test_df = train_test_split(temp_df, test_size=1/3, random_state=seed)
 
     # Output first column (IDs) for the training, validation, and test datasets to CSV files
-    train_df.iloc[:, 0].to_csv('dataAcquisition/Split_data_IDs/train_50k.csv', index=False)
-    valid_df.iloc[:, 0].to_csv('dataAcquisition/Split_data_IDs/valid_50k.csv', index=False)
-    test_df.iloc[:, 0].to_csv('dataAcquisition/Split_data_IDs/test_50k.csv', index=False)
+    train_df.iloc[:, 0].to_csv('dataAcquisition/Split_data_IDs/train_85k.csv', index=False)
+    valid_df.iloc[:, 0].to_csv('dataAcquisition/Split_data_IDs/valid_85k.csv', index=False)
+    test_df.iloc[:, 0].to_csv('dataAcquisition/Split_data_IDs/test_85k.csv', index=False)
 
 if __name__ == '__main__':
     main()
